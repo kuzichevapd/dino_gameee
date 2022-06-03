28a34e5ad2c48fca5b0dd17e3719dfad6ce0b678
kuzichevapd <kuzicheva.pd@edu.spbstu.ru>
2022-06-03 12:32:22 +0300

    i dont know what banch of changes it is, there have been too much

--- core/src/test/GameModelTest.java
+++ core/src/test/GameModelTest.java
@@ -19,16 +19,36 @@ public class GameModelTest {
     public void dinoJumpTest() {
         model.update(0);
         assertEquals(0, model.getHero().getVelocity().x);
-        assertEquals(Config.JUMP_POWER - Config.GRAVITY, model.getHero().getVelocity().y);
+        assertEquals(0, model.getHero().getVelocity().y);
+        model.startGame();
+        assertEquals(GameModel.GameState.RUN, model.getGameState());
+        model.doAction();
+        assertEquals(0, model.getHero().getVelocity().x);
+        assertEquals(Config.JUMP_POWER, model.getHero().getVelocity().y);
+
+
     }
 
     @Test
     public void startGameTest() {
         assertEquals(GameModel.GameState.START, model.getGameState());
-        model.update(0);
+        model.startGame();
         assertEquals(GameModel.GameState.RUN, model.getGameState());
     }
 
+    @Test
+    public void stopGameTest() {
+        model.stopGame();
+        assertEquals(GameModel.GameState.STOP, model.getGameState());
+    }
 
+    @Test
+    public void restartGameTest() {
+        model.restartGame();
+        assertEquals(GameModel.GameState.START, model.getGameState());
+        assertEquals(0, model.getGameTime());
+        assertEquals(Config.START_X, model.getHeroPositionX());
+        assertEquals(Config.GROUND_LEVEL, model.getHeroPositionY());
+    }
 
 }
